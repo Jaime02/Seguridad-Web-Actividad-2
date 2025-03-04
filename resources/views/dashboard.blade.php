2050417be9b6109baa35e0631fc20f55d3966b26
Jaime Resano <Jaime.Resano-Aisa@qt.io>
2025-03-04 16:59:16 +0100

    Aniadir tonterias a las vistas

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    @if (Auth::user()->isAdmin())
+                        Viendo el dashboard del Omnipotente "{{ Auth::user()->name }}"
+                    @else
+                        Viendo el dashboard de {{ Auth::user()->name }}
+                    @endif
                 </div>
             </div>
         </div>
